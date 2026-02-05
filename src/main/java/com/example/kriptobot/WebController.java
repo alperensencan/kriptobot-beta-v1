1e795321ab505381dcbbe5779b8b17df67a753da
Alperen Şencan <97888044+alperensencan@users.noreply.github.com>
2026-02-05 06:55:25 +0300

    Add files via upload

---
+++ src/main/java/com/example/kriptobot/WebController.java
@@ -0,0 +1,18 @@
+package com.example.kriptobot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+    @GetMapping("/")
+    public String root() {
+        return "redirect:/piyasa";
+    }
+
+    @GetMapping("/piyasa")
+    public String piyasa() {
+        return "forward:/piyasa.html";
+    }
+}
